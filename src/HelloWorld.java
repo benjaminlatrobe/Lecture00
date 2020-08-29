fd5f19465da9e278f76f532de7c7d14497ce3301
benlatrobe <benlatrobe@knights.ucf.edu>
2020-08-29 17:33:36 -0400

    First version

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("in main");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scan.next();
+
+        System.out.printf("%s", name);
+
+
+    }
+}
